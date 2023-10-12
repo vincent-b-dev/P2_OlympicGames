d37db8f73882a1fcf8aa76b540042bc8f9c875e9
unknown <jesapelgrub@gmail.com>
2023-10-12 18:23:53 +0200

    unsubscribe chart line compnent

--- src/app/components/chart-line/chart-line.component.ts
+++ src/app/components/chart-line/chart-line.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import DataLineChart from 'src/app/core/models/data-line-chart';
 
@@ -8,13 +9,14 @@ import DataLineChart from 'src/app/core/models/data-line-chart';
   templateUrl: './chart-line.component.html',
   styleUrls: ['./chart-line.component.scss'],
 })
-export class ChartLineComponent implements OnInit {
+export class ChartLineComponent implements OnInit, OnDestroy {
   @Input() id!: number;
   data: any;
   dataChart!: DataLineChart[];
   totalMedals!: number;
   totalAthletes!: number;
   totalParticipations!: number;
+  olympicSubscription!: Subscription;
 
   // options
   showLabels: boolean = true;
@@ -27,34 +29,40 @@ export class ChartLineComponent implements OnInit {
   constructor(private olympicService: OlympicService, private router: Router) {}
 
   ngOnInit(): void {
-    this.olympicService.getOlymppicById(this.id).subscribe((result) => {
-      if (result) {
-        this.data = result;
-        this.totalMedals = result.participations.reduce(
-          (total, medalsCount) => (total += medalsCount.medalsCount),
-          0
-        );
-        this.totalAthletes = result.participations.reduce(
-          (total, athletes) => (total += athletes.athleteCount),
-          0
-        );
-        const dataSeriesChart = result.participations.map((element) => {
-          const dataForChart: { name: string; value: number } = {
-            value: element.medalsCount,
-            name: element.year.toString(),
-          };
-          return dataForChart;
-        });
+    this.olympicSubscription = this.olympicService
+      .getOlymppicById(this.id)
+      .subscribe((result) => {
+        if (result) {
+          this.data = result;
+          this.totalMedals = result.participations.reduce(
+            (total, medalsCount) => (total += medalsCount.medalsCount),
+            0
+          );
+          this.totalAthletes = result.participations.reduce(
+            (total, athletes) => (total += athletes.athleteCount),
+            0
+          );
+          const dataSeriesChart = result.participations.map((element) => {
+            const dataForChart: { name: string; value: number } = {
+              value: element.medalsCount,
+              name: element.year.toString(),
+            };
+            return dataForChart;
+          });
 
-        this.dataChart = [
-          {
-            name: result.country,
-            series: dataSeriesChart,
-          },
-        ];
-      } else {
-        this.router.navigate(['/']);
-      }
-    });
+          this.dataChart = [
+            {
+              name: result.country,
+              series: dataSeriesChart,
+            },
+          ];
+        } else {
+          this.router.navigate(['/']);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.olympicSubscription && this.olympicSubscription.unsubscribe();
   }
 }
